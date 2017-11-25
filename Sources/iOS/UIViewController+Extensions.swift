0dd31969b2a42a96894093d252ba4d560a69af51
Khoa Pham <onmyway133@gmail.com>
2017-11-25 17:42:27 +0100

    Update

--- Sources/iOS/UIViewController+Extensions.swift
+++ Sources/iOS/UIViewController+Extensions.swift
@@ -4,10 +4,10 @@ extension UIViewController {
   /// Add a child view controller
   ///
   /// - Parameter chilController: The child view controller to add
-  public func omnia_add(chilController: UIViewController) {
-    addChildViewController(chilController)
-    view.addSubview(chilController.view)
-    chilController.didMove(toParentViewController: self)
+  public func omnia_add(childController: UIViewController) {
+    addChildViewController(childController)
+    view.addSubview(childController.view)
+    childController.didMove(toParentViewController: self)
   }
 
   /// Remove from the parent view controller
