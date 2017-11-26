1167a565507fccbc4002c9c71ff748186ed0cc29
Khoa Pham <onmyway133@gmail.com>
2017-11-26 22:32:03 +0100

    Add shuffle

---
+++ OmniaTests/Shared/Sequence+ExtensionsTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+import Omnia
+
+class Sequence_ExtensionsTests: XCTestCase {
+
+  func testShuffle() {
+    do {
+      let array = [1]
+      XCTAssertEqual(array.omnia_shuffled().count, 1)
+    }
+
+    do {
+      let array = Array(0..<10_000)
+      let _ = array.omnia_shuffled()
+    }
+  }
+}
---
+++ Sources/Shared/Sequence+Extensions.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+extension Sequence {
+
+  /// Shuffle sequence by creating a new array
+  ///
+  /// - Returns: Shuffled array
+  public func omnia_shuffled() -> [Element] {
+    var array = Array(self)
+
+    guard array.count > 1 else {
+      return array
+    }
+
+    let count = array.count
+    for i in 0..<(array.count - 1) {
+      let j = Int(arc4random_uniform(UInt32(count - i))) + i
+      array.swapAt(i, j)
+    }
+
+    return array
+  }
+}
