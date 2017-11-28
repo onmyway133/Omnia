4ac048d098f8afc1d8c6d998e680a0f3252a556f
Khoa Pham <onmyway133@gmail.com>
2017-11-28 21:44:50 +0100

    Update README

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Omnia
 
-[![CI Status](https://img.shields.io/circleci/project/github/onmyway133/Omnia.svg)](https://circleci.com/gh/onmyway133/Omnia)
 [![Version](https://img.shields.io/cocoapods/v/Omnia.svg?style=flat)](http://cocoadocs.org/docsets/Omnia)
 [![Carthage Compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
 [![License](https://img.shields.io/cocoapods/l/Omnia.svg?style=flat)](http://cocoadocs.org/docsets/Omnia)
 [![Platform](https://img.shields.io/cocoapods/p/Omnia.svg?style=flat)](http://cocoadocs.org/docsets/Omnia)
 ![Swift](https://img.shields.io/badge/%20in-swift%204.0-orange.svg)
+
+![](Screenshots/Artboard.png)
 
 ## Description
 
--- template.generated.swift
+++ template.generated.swift
@@ -3,4 +3,89 @@
 
 
 
-hello
+## Data
+
+```swift
+omnia_hexString
+```
+
+
+## Optional
+
+```swift
+omnia_isPresent
+omnia_unwrap(message: String)
+```
+
+
+## Sequence
+
+```swift
+omnia_shuffled()
+```
+
+
+## String
+
+```swift
+omnia_capitalizedFirstLetter
+omnia_localized
+```
+
+
+## UICollectionView
+
+```swift
+omnia_register<T: UICollectionViewCell>(cellType: T.Type)
+omnia_dequeue<T: UICollectionViewCell>(for indexPath: IndexPath)
+```
+
+
+## UIColor
+
+```swift
+init(omnia_hex hex: String)
+omnia_toHSL()
+init(omnia_hslColor hslColor: HSLColor)
+omnia_random()
+omnia_darken(amount: CGFloat = 0.2)
+omnia_lighten(amount: CGFloat = 0.2)
+```
+
+
+## UIImage
+
+```swift
+init?(omnia_color color: UIColor, size: CGSize)
+```
+
+
+## UIScrollView
+
+```swift
+omnia_scrollToBottom()
+```
+
+
+## UITableView
+
+```swift
+omnia_register<T: UITableViewCell>(cellType: T.Type)
+omnia_dequeue<T: UITableViewCell>(for indexPath: IndexPath)
+```
+
+
+## UIView
+
+```swift
+omnia_toImage()
+```
+
+
+## UIViewController
+
+```swift
+omnia_add(childController: UIViewController)
+omnia_remove()
+```
+
