5f4552e0e62b98adf9b7c3753c70f1c5e6276a51
Khoa Pham <onmyway133@gmail.com>
2017-11-28 22:22:30 +0100

    Add more test

--- OmniaTests/Shared/Comparable+ExtensionsTests.swift
+++ OmniaTests/Shared/Comparable+ExtensionsTests.swift
@@ -6,5 +6,11 @@ class Comparable_ExtensionsTests: XCTestCase {
     XCTAssertTrue(5.omnia_isBetween(min: 1, max: 10))
     XCTAssertTrue(5.omnia_isBetween(min: 5, max: 10))
     XCTAssertFalse(5.5.omnia_isBetween(min: 5.6, max: 5.7))
+
+    do {
+      let min = Date(timeIntervalSinceNow: -10)
+      let max = Date(timeIntervalSinceNow: 10)
+      XCTAssertTrue(Date().omnia_isBetween(min: min, max: max))
+    }
   }
 }
