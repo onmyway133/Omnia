9684f5eee5fdb3df4f6ade992a91f9e5ec54ca35
Khoa Pham <onmyway133@gmail.com>
2019-04-11 14:26:50 +0200

    Add date extension

---
+++ OmniaTests/Shared/DateTests.swift
@@ -0,0 +1,25 @@
+//
+//  DateTests.swift
+//  Omnia
+//
+//  Created by khoa on 11/04/2019.
+//  Copyright © 2019 Khoa Pham. All rights reserved.
+//
+
+import XCTest
+import Omnia
+
+class DateTests: XCTestCase {
+
+    func testComponent() {
+        let date = Date(timeIntervalSince1970: 1554985373)
+        
+        XCTAssert(date.second == 53)
+        XCTAssert(date.minute == 22)
+        XCTAssert(date.hour == 14)
+        XCTAssert(date.day == 11)
+        XCTAssert(date.month == 4)
+        XCTAssert(date.year == 2019)
+        XCTAssert(date.weekday == 5)
+    }
+}
---
+++ Sources/Shared/Date.swift
@@ -0,0 +1,40 @@
+//
+//  Date.swift
+//  Omnia
+//
+//  Created by khoa on 11/04/2019.
+//  Copyright © 2019 Khoa Pham. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+
+    var second: Int {
+        return Calendar.current.component(.second, from: self)
+    }
+    
+    var minute: Int {
+        return Calendar.current.component(.minute, from: self)
+    }
+    
+    var hour: Int {
+        return Calendar.current.component(.hour, from: self)
+    }
+    
+    var day: Int {
+        return Calendar.current.component(.day, from: self)
+    }
+    
+    var month: Int {
+        return Calendar.current.component(.month, from: self)
+    }
+    
+    var year: Int {
+        return Calendar.current.component(.year, from: self)
+    }
+    
+    var weekday: Int {
+        return Calendar.current.component(.weekday, from: self)
+    }
+}
