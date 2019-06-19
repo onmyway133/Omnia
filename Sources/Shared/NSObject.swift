c21adbd6f109bd41f668b0c77e949b1ea80b6ebc
Khoa <onmyway133@gmail.com>
2019-06-19 14:31:44 +0200

    Update NSObject.swift

--- Sources/Shared/NSObject.swift
+++ Sources/Shared/NSObject.swift
@@ -7,25 +7,8 @@
 
 import Foundation
 
-/// Add-on utitlies to all types
-/// https://github.com/JetBrains/kotlin/blob/master/libraries/stdlib/src/kotlin/util/Standard.kt
-protocol TypeAddOnAware {}
-
-extension TypeAddOnAware {
-    /// Calls the specified closure with this instance as its receiver
-    /// and returns this instance
-    ///
-    /// - Parameter closure: The closure to apply
-    func apply(closure: (Self) -> Void) {
-        closure(self)
-    }
-}
-
-/// Conform NSObject by default
-extension NSObject: TypeAddOnAware {}
-
 extension NSObject {
-    func with(_ closure: (NSObject) -> Void) {
+    func apply(_ closure: (NSObject) -> Void) {
         closure(self)
     }
 }
