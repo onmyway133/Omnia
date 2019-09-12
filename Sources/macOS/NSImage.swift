486ceca5e07944d322d98d54b4e8f9451935f2c4
Khoa Pham <onmyway133@gmail.com>
2019-09-12 10:19:35 +0200

    Add NSImage

---
+++ Sources/macOS/NSImage.swift
@@ -0,0 +1,24 @@
+//
+//  NSImage.swift
+//  Omnia-macOS
+//
+//  Created by khoa on 12/09/2019.
+//  Copyright © 2019 Khoa Pham. All rights reserved.
+//
+
+import AppKit
+
+public extension NSImage {
+    func tint(color: NSColor) -> NSImage {
+        let image = self.copy() as! NSImage
+        image.lockFocus()
+
+        color.set()
+        let imageRect = NSRect(origin: .zero, size: image.size)
+        imageRect.fill(using: .sourceAtop)
+
+        image.unlockFocus()
+
+        return image
+    }
+}
