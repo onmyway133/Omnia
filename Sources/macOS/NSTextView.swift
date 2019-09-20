8302a109b66666c8479ecf58ad740f112df73613
Khoa Pham <onmyway133@gmail.com>
2019-09-20 10:54:03 +0200

    Add NSTextView

---
+++ Sources/macOS/NSTextView.swift
@@ -0,0 +1,18 @@
+//
+//  NSTextView.swift
+//  Omnia-macOS
+//
+//  Created by khoa on 20/09/2019.
+//  Copyright © 2019 Khoa Pham. All rights reserved.
+//
+
+import AppKit
+
+public extension NSTextView {
+    func styleAsLabel(color: NSColor, font: NSFont) {
+        textColor = color
+        self.font = font
+        isEditable = false
+        backgroundColor = NSColor.clear
+    }
+}
