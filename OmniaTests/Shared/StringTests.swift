9504a8f0f3ac1ab55009c84bb4779f7797de414a
Khoa Pham <onmyway133@gmail.com>
2019-10-02 15:57:22 +0200

    Update test

--- OmniaTests/Shared/StringTests.swift
+++ OmniaTests/Shared/StringTests.swift
@@ -23,9 +23,9 @@ class StringTests: XCTestCase {
     }
 
     func testTrimmingInside() {
-        XCTAssertEqual("Hello (1) world".trimmingInsideParenthesis(), "Hello  world")
-        XCTAssertEqual(#"Hello "1" world"#.trimmingInsideQuotes(), "Hello  world")
-        XCTAssertEqual(#"Hello “1” world"#.trimmingInsideSmartQuotes(), "Hello  world")
+        XCTAssertEqual("Hello (1) world".trimmingInsideParenthesis(), "Hello world")
+        XCTAssertEqual(#"Hello "1" world"#.trimmingInsideQuotes(), "Hello world")
+        XCTAssertEqual(#"Hello “1” world"#.trimmingInsideSmartQuotes(), "Hello world")
     }
 }
 
