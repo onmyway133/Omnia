99082ba75887a274ff4d26775d5d3119848b8586
Khoa Pham <onmyway133@gmail.com>
2019-11-13 14:01:06 +0100

    Use IfLet

--- Sources/SwiftUI/View/ActivittyIndicator.swift
+++ Sources/SwiftUI/View/ActivittyIndicator.swift
@@ -10,7 +10,7 @@
 
 import SwiftUI
 
-@available(iOS 13.0, *)
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public struct ActivityIndicator: UIViewRepresentable {
     @Binding var isAnimating: Bool
     let style: UIActivityIndicatorView.Style
