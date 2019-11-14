f6f29c604656d89326bc31736ffccd98e69fc61c
Khoa Pham <onmyway133@gmail.com>
2019-11-14 13:02:23 +0100

    list color

---
+++ Sources/Shared/Function.swift
@@ -0,0 +1,58 @@
+//
+//  Color.swift
+//  Omnia
+//
+//  Created by khoa on 14/11/2019.
+//  Copyright © 2019 Khoa Pham. All rights reserved.
+//
+
+import Foundation
+
+// https://www.webnots.com/flat-ui-color-codes/
+public func flatColorsInHex() -> [String] {
+    /*
+     TURQUOISE    26,188,156        #1ABC9C
+     GREEN SEA    22,160,133        #16A085
+     EMERALD    46,204,113          #2ECC71
+     NEPHRITIS    39,174,96         #27AE60
+     PETER RIVER    52,152,219      #3498DB
+     BELIZE HOLE    41,128,185      #2980B9
+     AMETHYST    155,89,182         #9B59B6
+     WISTERIA    142,68,173         #8E44AD
+     WET ASPHALT    52,73,94        #34495E
+     MIDNIGHT BLUE    44,62,80      #2C3E50
+     SUN FLOWER    241,196,15       #F1C40F
+     ORANGE    243,156,18           #F39C12
+     CARROT    230,126,34           #E67E22
+     PUMPKIN    211,84,0            #D35400
+     ALIZARIN    231,76,60          #E74C3C
+     POMEGRANATE    192,57,43       #C0392B
+     CLOUDS    236,240,241          #ECF0F1
+     SILVER    189,195,199          #BDC3C7
+     CONCRETE    149,165,166        #95A5A6
+     ASBESTOS    127,140,141        #7F8C8D
+    */
+
+    return [
+        "#1ABC9C",
+        "#16A085",
+        "#2ECC71",
+        "#27AE60",
+        "#3498DB",
+        "#2980B9",
+        "#9B59B6",
+        "#8E44AD",
+        "#34495E",
+        "#2C3E50",
+        "#F1C40F",
+        "#F39C12",
+        "#E67E22",
+        "#D35400",
+        "#E74C3C",
+        "#C0392B",
+        "#ECF0F1",
+        "#BDC3C7",
+        "#95A5A6",
+        "#7F8C8D"
+    ]
+}
