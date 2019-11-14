1b4e8bc3c29274ff0d6818506fd658c7c406b5ff
Khoa Pham <onmyway133@gmail.com>
2019-11-14 12:16:55 +0100

    Color

---
+++ Sources/SwiftUI/Extensions/Color.swift
@@ -0,0 +1,27 @@
+//
+//  Color.swift
+//  Omnia
+//
+//  Created by khoa on 14/11/2019.
+//  Copyright © 2019 Khoa Pham. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+public extension SwiftUI.Color {
+    init(hex: String) {
+        let hex = hex.replacingOccurrences(of: "#", with: "")
+
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+
+        self.init(red: Double(r) / 0xff, green: Double(g) / 0xff, blue: Double(b) / 0xff)
+    }
+}
