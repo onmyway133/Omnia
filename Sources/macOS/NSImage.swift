4bb6d78df44529e953a1d84e5b86a1c60c12b31c
Khoa Pham <onmyway133@gmail.com>
2020-04-05 21:14:07 +0200

    Update

--- Sources/macOS/NSImage.swift
+++ Sources/macOS/NSImage.swift
@@ -23,6 +23,23 @@ public extension NSImage {
 
         return image
     }
+
+    func resizeImage(_ width: CGFloat, _ height: CGFloat) -> NSImage {
+        let img = NSImage(size: CGSize(width:width, height:height))
+
+        img.lockFocus()
+        let ctx = NSGraphicsContext.current
+        ctx?.imageInterpolation = .high
+        self.draw(
+            in: NSMakeRect(0, 0, width, height),
+            from: NSMakeRect(0, 0, size.width, size.height),
+            operation: .copy,
+            fraction: 1
+        )
+        img.unlockFocus()
+
+        return img
+    }
 }
 
 #endif
