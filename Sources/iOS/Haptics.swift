9387a107fdcfdd19a821e09f8340f9d37c55f82f
Khoa Pham <onmyway133@gmail.com>
2021-05-14 11:08:50 +0200

    Feedback

--- Sources/iOS/Haptics.swift
+++ Sources/iOS/Haptics.swift
@@ -11,16 +11,19 @@ import UIKit
 public struct Haptics {
     public static func selection() {
         let generator = UISelectionFeedbackGenerator()
+        generator.prepare()
         generator.selectionChanged()
     }
     
     public static func impact(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
         let generator = UIImpactFeedbackGenerator(style: style)
+        generator.prepare()
         generator.impactOccurred()
     }
     
     public static func notification(_ style: UINotificationFeedbackGenerator.FeedbackType) {
         let generator = UINotificationFeedbackGenerator()
+        generator.prepare()
         generator.notificationOccurred(style)
     }
 }
