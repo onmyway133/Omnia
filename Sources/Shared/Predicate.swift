4ab128cb4a1b57d457bd300ffe7876f18766676e
Khoa Pham <onmyway133@gmail.com>
2021-11-11 20:48:19 +0100

    predicate

---
+++ Sources/Shared/Predicate.swift
@@ -0,0 +1,40 @@
+//
+//  Predicate.swift
+//  
+//
+//  Created by khoa on 11/11/2021.
+//
+
+import Foundation
+
+public extension NSPredicate {
+    static func and(_ predicates: [NSPredicate]) -> NSPredicate {
+        NSCompoundPredicate(type: .and, subpredicates: predicates)
+    }
+
+    static func or(_ predicates: [NSPredicate]) -> NSPredicate {
+        NSCompoundPredicate(type: .or, subpredicates: predicates)
+    }
+
+    static func not(_ predicates: [NSPredicate]) -> NSPredicate {
+        NSCompoundPredicate(type: .not, subpredicates: predicates)
+    }
+
+    static func compare<Root, V>(
+        _ kp: KeyPath<Root, V>,
+        op: NSComparisonPredicate.Operator,
+        value: Any
+    ) -> NSPredicate {
+        let ex1 = \Root.self == kp
+            ? NSExpression.expressionForEvaluatedObject()
+            : NSExpression(forKeyPath: kp)
+        let ex2 = NSExpression(forConstantValue: value)
+
+        return NSComparisonPredicate(
+            leftExpression: ex1,
+            rightExpression: ex2,
+            modifier: .direct,
+            type: op
+        )
+    }
+}
